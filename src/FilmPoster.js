ca1d6f2e4f6406c70b15033a1b8f6363fa429e5e
Scott <scrosehart@gmail.com>
2018-03-13 12:27:00 -0700

    added final changed

---
+++ src/FilmPoster.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const FilmPoster = props => {
+
+  const { url, title } = props
+  const posterUrl = 'https://image.tmdb.org/t/p/w780/' + url
+
+  return (
+    <img src={posterUrl} alt={title} />
+  )
+}
+
+export default FilmPoster
---
+++ src/FilmRow.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import FilmPoster from './FilmPoster';
+import Fave from './Fave';
+
+const FilmRow = props => {
+  const { title, date, url } = props
+
+  const posterUrl = 'https://image.tmdb.org/t/p/w780/' + url
+  const year = new Date(date).getFullYear()
+
+  return(
+    <div className='film-row' onClick={props.onDetailsClick}>
+      <FilmPoster title={title} url={url} />
+      <div className="film-summary">
+        <h1>{title}</h1>
+        <p>{year}</p>
+      </div>
+      <Fave isFave={props.isFave} onFaveToggle={props.onFaveToggle} />
+    </div>
+  )
+}
+
+export default FilmRow;
